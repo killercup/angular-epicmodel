88364833ebb0abc299ff91c283094176f297d13d
Pascal Hertleif <killercup@gmail.com>
2014-02-07 14:40:30 +0100

    Generated documentation for 9eb19a1c885fe2dc471ce5b27f9d0322ba4dfdf4

---
+++ toc.js
@@ -0,0 +1,266 @@
+window.files = [
+{
+  "path": "index.html",
+  "originalName": "README.md",
+  "originalPath": "README.md",
+  "name": "index.html",
+  "title": "EpicModel",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "epicmodel",
+      "title": "EpicModel"
+    },
+    {
+      "level": 2,
+      "slug": "install",
+      "title": "Install"
+    },
+    {
+      "level": 3,
+      "slug": "dependencies",
+      "title": "Dependencies"
+    },
+    {
+      "level": 2,
+      "slug": "usage",
+      "title": "Usage"
+    },
+    {
+      "level": 2,
+      "slug": "features",
+      "title": "Features"
+    }
+  ]
+},
+{
+  "path": "src/model.coffee.html",
+  "originalName": "model.coffee",
+  "originalPath": "src/model.coffee",
+  "name": "model.coffee.html",
+  "lang": "coffeescript",
+  "title": "Epic Model",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "epic-model",
+      "title": "Epic Model"
+    },
+    {
+      "level": 2,
+      "slug": "global-config",
+      "title": "Global Config"
+    },
+    {
+      "level": 2,
+      "slug": "constructor",
+      "title": "Constructor"
+    },
+    {
+      "level": 3,
+      "slug": "options",
+      "title": "Options"
+    },
+    {
+      "level": 4,
+      "slug": "storage-implementation",
+      "title": "Storage Implementation"
+    },
+    {
+      "level": 3,
+      "slug": "in-memory-data",
+      "title": "In Memory Data"
+    },
+    {
+      "level": 3,
+      "slug": "http-requests-and-stuff",
+      "title": "HTTP Requests and Stuff"
+    },
+    {
+      "level": 3,
+      "slug": "generic-getters",
+      "title": "Generic Getters"
+    },
+    {
+      "level": 3,
+      "slug": "mixin-extras",
+      "title": "Mixin Extras"
+    },
+    {
+      "level": 2,
+      "slug": "storage-wrapper-for-localstorage",
+      "title": "Storage Wrapper for LocalStorage"
+    },
+    {
+      "level": 3,
+      "slug": "get-item",
+      "title": "Get Item"
+    },
+    {
+      "level": 3,
+      "slug": "set-item",
+      "title": "Set Item"
+    },
+    {
+      "level": 3,
+      "slug": "remove-item",
+      "title": "Remove Item"
+    }
+  ]
+},
+{
+  "path": "test/helpers.coffee.html",
+  "originalName": "helpers.coffee",
+  "originalPath": "test/helpers.coffee",
+  "name": "helpers.coffee.html",
+  "lang": "coffeescript",
+  "title": "Helpers",
+  "toc": [
+    {
+      "level": 1,
+      "slug": "helpers",
+      "title": "Helpers"
+    }
+  ]
+},
+{
+  "path": "test/karma.conf.coffee.html",
+  "originalName": "karma.conf.coffee",
+  "originalPath": "test/karma.conf.coffee",
+  "name": "karma.conf.coffee.html",
+  "lang": "coffeescript",
+  "toc": []
+},
+{
+  "path": "test/unit/basic_spec.coffee.html",
+  "originalName": "basic_spec.coffee",
+  "originalPath": "test/unit/basic_spec.coffee",
+  "name": "basic_spec.coffee.html",
+  "lang": "coffeescript",
+  "toc": []
+},
+{
+  "path": "test/unit/config_spec.coffee.html",
+  "originalName": "config_spec.coffee",
+  "originalPath": "test/unit/config_spec.coffee",
+  "name": "config_spec.coffee.html",
+  "lang": "coffeescript",
+  "toc": []
+},
+{
+  "path": "test/unit/extras_spec.coffee.html",
+  "originalName": "extras_spec.coffee",
+  "originalPath": "test/unit/extras_spec.coffee",
+  "name": "extras_spec.coffee.html",
+  "lang": "coffeescript",
+  "toc": []
+},
+{
+  "path": "test/unit/global_config_spec.coffee.html",
+  "originalName": "global_config_spec.coffee",
+  "originalPath": "test/unit/global_config_spec.coffee",
+  "name": "global_config_spec.coffee.html",
+  "lang": "coffeescript",
+  "toc": []
+},
+{
+  "path": "test/unit/list_detail_resource_spec.coffee.html",
+  "originalName": "list_detail_resource_spec.coffee",
+  "originalPath": "test/unit/list_detail_resource_spec.coffee",
+  "name": "list_detail_resource_spec.coffee.html",
+  "lang": "coffeescript",
+  "toc": [
+    {
+      "level": 3,
+      "slug": "mock-server-on-messages",
+      "title": "Mock Server on /messages"
+    },
+    {
+      "level": 3,
+      "slug": "initialize-new-collection-each-time",
+      "title": "Initialize new Collection each time"
+    },
+    {
+      "level": 3,
+      "slug": "get-messages",
+      "title": "GET /messages"
+    },
+    {
+      "level": 3,
+      "slug": "query-cached-data",
+      "title": "Query cached data"
+    },
+    {
+      "level": 3,
+      "slug": "get-messages-2",
+      "title": "GET /messages/2"
+    },
+    {
+      "level": 3,
+      "slug": "post-messages-1",
+      "title": "POST /messages/1"
+    },
+    {
+      "level": 3,
+      "slug": "delete-messages-2",
+      "title": "DELETE /messages/2"
+    },
+    {
+      "level": 3,
+      "slug": "post-messages",
+      "title": "POST /messages"
+    }
+  ]
+},
+{
+  "path": "test/unit/local_storage_spec.coffee.html",
+  "originalName": "local_storage_spec.coffee",
+  "originalPath": "test/unit/local_storage_spec.coffee",
+  "name": "local_storage_spec.coffee.html",
+  "lang": "coffeescript",
+  "toc": []
+},
+{
+  "path": "test/unit/request_config_spec.coffee.html",
+  "originalName": "request_config_spec.coffee",
+  "originalPath": "test/unit/request_config_spec.coffee",
+  "name": "request_config_spec.coffee.html",
+  "lang": "coffeescript",
+  "toc": [
+    {
+      "level": 3,
+      "slug": "mock-server-on-items",
+      "title": "Mock Server on /items"
+    },
+    {
+      "level": 2,
+      "slug": "list-resource",
+      "title": "List Resource"
+    },
+    {
+      "level": 2,
+      "slug": "detail-resource",
+      "title": "Detail Resource"
+    }
+  ]
+},
+{
+  "path": "test/unit/singleton_resource_spec.coffee.html",
+  "originalName": "singleton_resource_spec.coffee",
+  "originalPath": "test/unit/singleton_resource_spec.coffee",
+  "name": "singleton_resource_spec.coffee.html",
+  "lang": "coffeescript",
+  "toc": [
+    {
+      "level": 3,
+      "slug": "mock-server-on-me",
+      "title": "Mock Server on /me"
+    },
+    {
+      "level": 3,
+      "slug": "initialize-new-collection-each-time",
+      "title": "Initialize new Collection each time"
+    }
+  ]
+}
+];
